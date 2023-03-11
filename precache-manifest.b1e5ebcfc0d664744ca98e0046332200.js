3f8241fd261495a363052c13f55221e9c039ccba
mateuszxstepien <mateuszxstepien@gmail.com>
2023-03-11 21:06:52 +0100

    Updates

---
+++ precache-manifest.b1e5ebcfc0d664744ca98e0046332200.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "311e688513f2dc1d3966",
+    "url": "/routing-site/static/css/main.c607a1e9.chunk.css"
+  },
+  {
+    "revision": "311e688513f2dc1d3966",
+    "url": "/routing-site/static/js/main.311e6885.chunk.js"
+  },
+  {
+    "revision": "b3c57f7c4d08aa0fe6c9",
+    "url": "/routing-site/static/js/1.b3c57f7c.chunk.js"
+  },
+  {
+    "revision": "efaad46941035a077ef2",
+    "url": "/routing-site/static/js/runtime~main.efaad469.js"
+  },
+  {
+    "revision": "5ae4622f445d34aa6f3f9c3ac8567e0a",
+    "url": "/routing-site/static/media/img1.5ae4622f.jpg"
+  },
+  {
+    "revision": "08ffa916d07b2e5836514c15e88f1b6f",
+    "url": "/routing-site/static/media/img2.08ffa916.jpg"
+  },
+  {
+    "revision": "2db9c939c3b6ca923a6daf90e51e177a",
+    "url": "/routing-site/static/media/img3.2db9c939.jpg"
+  },
+  {
+    "revision": "4fa7bd0e870eebf16473d2657cd64e38",
+    "url": "/routing-site/index.html"
+  }
+];
